bdb051af4e34acf411c06153ad2fac5cce2a016b
Erick_Dev <erick@gmail.com>
2026-04-28 09:04:27 -0600

    feature0 commit 5

--- Principal.py
+++ Principal.py
@@ -6,3 +6,11 @@ class E():
     def Mostrar(self):
         print(f'Hola E')
 
+class B(E):
+    def Mostrar(self):
+        print(f'Hola B')
+
+class C(A):
+    def Mostrar(self):
+        print(f'Hola C')
+
